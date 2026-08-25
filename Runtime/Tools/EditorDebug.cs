e32a2c9bbe083d285f8f9085cd19fbdd076053df
Ilia Petrov-Komotskii <ilia@inkedkettle.art>
2026-08-25 13:06:18 +0200

    feat: Adedd EditorDebug static class

---
+++ Runtime/Tools/EditorDebug.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Mane.Unity
+{
+    public static class EditorDebug
+    {
+        public static void Log(object message)
+        {
+#if UNITY_EDITOR
+            Debug.Log(message);
+#endif
+        }
+        
+        public static void LogWarning(object message)
+        {
+#if UNITY_EDITOR
+            Debug.LogWarning(message);
+#endif
+        }
+        
+        public static void LogError(object message)
+        {
+#if UNITY_EDITOR
+            Debug.LogError(message);
+#endif
+        }
+    }
+}
